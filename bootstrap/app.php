25229a124df0d4bc29c56ed91ca6891b5ab71320
memoowi <megatenlike@gmail.com>
2026-02-16 04:25:13 +0700

    fix: error response on unauthenticated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,15 @@ use Illuminate\Validation\ValidationException;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(web: __DIR__ . '/../routes/web.php', api: __DIR__ . '/../routes/api.php', commands: __DIR__ . '/../routes/console.php', health: '/up')
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->redirectGuestsTo(
+            fn() => response()->json(
+                [
+                    'status' => 'Error',
+                    'message' => 'Unauthenticated.',
+                ],
+                401,
+            ),
+        );
         $middleware->alias([
             'role' => App\Http\Middleware\RoleMiddleware::class,
         ]);
