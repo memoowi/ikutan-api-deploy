b3d3533c81e1f65fe2c0e1afec9463d9ace83266
memoowi <megatenlike@gmail.com>
2026-02-16 17:25:59 +0700

    fix: docs to be able acces by guest

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Dedoc\Scramble\Scramble;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Allow the docs to be able access by public
+        Gate::define('viewApiDocs', function (User|null $user = null) {
+            return true;
+        });
+        // Only Expose the UI
+        Scramble::configure()->expose(
+            ui: '/',
+            // document: '/docs/api.json',
+        );
     }
 }
